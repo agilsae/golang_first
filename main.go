e8258d47bcc563aa3ba98cfa9b9052510868ab8a
agilsaebfi <agil.sae@bfi.co.id>
2023-07-03 14:33:18 +0700

    final feature auth api

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"diary-api/controller"
 	"diary-api/database"
+	"diary-api/middleware"
 	"diary-api/model"
+	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
     loadEnv()
     loadDatabase()
+    serveApplication()
+}
+
+func loadEnv() {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 }
 
 func loadDatabase() {
@@ -19,9 +31,18 @@ func loadDatabase() {
     database.Database.AutoMigrate(&model.Entry{})
 }
 
-func loadEnv() {
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+func serveApplication() {
+    router := gin.Default()
+
+    publicRoutes := router.Group("/auth")
+    publicRoutes.POST("/register", controller.Register)
+    publicRoutes.POST("/login", controller.Login)
+
+    protectedRoutes := router.Group("/api")
+    protectedRoutes.Use(middleware.JWTAuthMiddleware())
+    protectedRoutes.POST("/entry", controller.AddEntry)
+    protectedRoutes.GET("/entry", controller.GetAllEntries)
+
+    router.Run(":8000")
+    fmt.Println("Server running on port 8000")
 }
